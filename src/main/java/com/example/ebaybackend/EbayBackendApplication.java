2b4b13094a5aa3018b81b238b7dd9bde3e64fdbb
MichalJanosik <janosik.michal.jm@gmail.com>
2022-08-16 23:56:27 +0200

    create project

---
+++ src/main/java/com/example/ebaybackend/EbayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ebaybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbayBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbayBackendApplication.class, args);
+    }
+
+}

8383b9762d229f6aa5b0af64b348408ea931c0cf
MichalJanosik <janosik.michal.jm@gmail.com>
2022-10-20 10:56:08 +0200

    Ebay-registration updated getRegistrationDTO in UserService

--- src/test/java/com/example/ebaybackend/service/RoleServiceImplUnitTest.java
+++ src/test/java/com/example/ebaybackend/service/RoleServiceImplUnitTest.java
@@ -2,16 +2,12 @@ package com.example.ebaybackend.service;
 
 import com.example.ebaybackend.model.Role;
 import com.example.ebaybackend.repository.RoleRepository;
-import net.bytebuddy.dynamic.DynamicType;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,11 +16,18 @@ import static org.mockito.ArgumentMatchers.any;
 @ExtendWith(MockitoExtension.class)
 class RoleServiceImplUnitTest {
 
+    private static RoleRepository mockedRoleRepository;
+
+    @BeforeAll
+    static void initialSetup() {
+        mockedRoleRepository = Mockito.mock(RoleRepository.class);
+    }
+
     @Test
-    void saveRole() {
+    void saveRole_OK() {
         Role role = new Role("ROLE_USER");
         Role savedRole;
-        RoleRepository mockedRoleRepository = Mockito.mock(RoleRepository.class);
+
         Mockito.when(mockedRoleRepository.save(any(Role.class)))
                 .thenReturn(role);
 
@@ -35,13 +38,26 @@ class RoleServiceImplUnitTest {
     }
 
     @Test
-    void findRoleByName() {
+    void findRoleByName_OK() {
         String roleName = "ROLE_USER";
         Role role = new Role(roleName);
         Optional<Role> roleOptional = Optional.of(role);
 
-        RoleRepository mockedRoleRepository = Mockito.mock(RoleRepository.class);
-        Mockito.when(mockedRoleRepository.findRoleByName(roleName)).thenReturn(roleOptional);
+        Mockito.when(mockedRoleRepository.findRoleByName(roleName))
+                .thenReturn(roleOptional);
+
+        Optional<Role> roleFound = mockedRoleRepository.findRoleByName("ROLE_USER");
+
+        assertEquals(roleName, roleFound.get().getName());
+    }
+
+    @Test
+    void findRoleByName_NULL() {
+        String roleName = "ROLE_USER";
+        Optional<Role> roleOptional = Optional.empty();
+
+        Mockito.when(mockedRoleRepository.findRoleByName(roleName))
+                .thenReturn(roleOptional);
 
         Optional<Role> roleFound = mockedRoleRepository.findRoleByName("ROLE_USER");
 

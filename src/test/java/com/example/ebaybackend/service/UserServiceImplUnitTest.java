e79ccf85a7a604be55223bcc7bdc6171683530cd
MichalJanosik <janosik.michal.jm@gmail.com>
2022-10-20 09:48:20 +0200

    Ebay-registration merge main and add UserServiceImplUnitTest.class

---
+++ src/test/java/com/example/ebaybackend/service/UserServiceImplUnitTest.java
@@ -0,0 +1,24 @@
+package com.example.ebaybackend.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplUnitTest {
+
+    @Test
+    void getRegistrationDTO() {
+    }
+
+    @Test
+    void saveNewUser() {
+    }
+
+    @Test
+    void addRoleToUser() {
+    }
+
+    @Test
+    void loadUserByUsername() {
+    }
+}
